649b0630343ae66a10170fbd38dfef4e847db38b
umar365 <81367661+umar365@users.noreply.github.com>
2024-10-03 00:45:19 +0600

    Update Ping.py

--- Ping.py
+++ Ping.py
@@ -6,8 +6,7 @@ def keep_alive(url):
             response = requests.get(url)
             print(f"Sent request to {url}: {response.status_code}")
         except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-        time.sleep(60)  # Wait for 60 seconds before the next request
+            print(f"Error: {e}") 
 
 if __name__ == "__main__":
     target_url = "https://sabits-website.onrender.com"  # Change this to your website
